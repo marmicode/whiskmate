753ed7343f878922da7aead7a23d44a093dd969a
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-31 16:50:17 +0200

    🚧 setup <wm-recipe-search> integration test

---
+++ apps/whiskmate/src/app/recipe/recipe-search.component.integration.spec.ts
@@ -0,0 +1,37 @@
+import { RecipeRepository } from './recipe-repository.service';
+import { TestBed } from '@angular/core/testing';
+import { RecipeSearchComponent } from './recipe-search.component';
+import { Recipe } from './recipe';
+
+describe(RecipeSearchComponent.name, () => {
+  const papperdelle = { id: 'papperdelle-with-rose-harissa' } as Recipe;
+  const puyLentil = { id: 'puy-lentil-and-aubergine-stew' } as Recipe;
+
+  it.todo('🚧 should search recipes without keyword on load');
+
+  async function createComponent() {
+    const mockSearch = jest.fn() as jest.MockedFunction<
+      typeof RecipeRepository.prototype.search
+    >;
+
+    await TestBed.configureTestingModule({
+      // @todo
+      providers: [
+        {
+          provide: RecipeRepository,
+          useValue: {
+            search: mockSearch,
+          },
+        },
+      ],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(RecipeSearchComponent);
+
+    return {
+      component: fixture.componentInstance,
+      fixture,
+      mockSearch,
+    };
+  }
+});

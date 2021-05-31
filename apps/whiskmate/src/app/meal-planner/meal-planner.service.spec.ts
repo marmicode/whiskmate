4abf5e9ac55f999980561c66b31a9f1ae8e9c56b
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-31 11:40:07 +0200

    ✅ make MealPlanner reactive

--- apps/whiskmate/src/app/meal-planner/meal-planner.service.spec.ts
+++ apps/whiskmate/src/app/meal-planner/meal-planner.service.spec.ts
@@ -18,6 +18,45 @@ describe(MealPlanner.name, () => {
     ]);
   });
 
+  it('should watch recipes', () => {
+    const observer = jest.fn();
+    mealPlanner.recipes$.subscribe(observer);
+
+    mealPlanner.addRecipe(papperdelle);
+    mealPlanner.addRecipe(puyLentil);
+
+    expect(observer).toBeCalledTimes(3);
+    expect(observer).toHaveBeenNthCalledWith(1, []);
+    expect(observer).toHaveBeenNthCalledWith(2, [
+      expect.objectContaining({ id: 'papperdelle-with-rose-harissa' }),
+    ]);
+    expect(observer).toHaveBeenNthCalledWith(3, [
+      expect.objectContaining({ id: 'papperdelle-with-rose-harissa' }),
+      expect.objectContaining({ id: 'puy-lentil-and-aubergine-stew' }),
+    ]);
+  });
+
+  it('should watch if recipe can be added', () => {
+    const observer = jest.fn();
+    mealPlanner.watchCanAddRecipe(papperdelle).subscribe(observer);
+
+    mealPlanner.addRecipe(papperdelle);
+
+    expect(observer).toBeCalledTimes(2);
+    expect(observer).toBeCalledWith(true);
+    expect(observer).toBeCalledWith(false);
+  });
+
+  it("should not trigger observable if result didn't change", () => {
+    const observer = jest.fn();
+    mealPlanner.watchCanAddRecipe(papperdelle).subscribe(observer);
+
+    mealPlanner.addRecipe(papperdelle);
+    mealPlanner.addRecipe(puyLentil);
+
+    expect(observer).toBeCalledTimes(2);
+  });
+
   describe('with recipe', () => {
     beforeEach(() => {
       mealPlanner.addRecipe(papperdelle);
--- apps/whiskmate/src/app/meal-planner/meal-planner.service.ts
+++ apps/whiskmate/src/app/meal-planner/meal-planner.service.ts
@@ -1,3 +1,5 @@
+import { distinctUntilChanged, map } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipe/recipe';
 
@@ -5,20 +7,49 @@ import { Recipe } from '../recipe/recipe';
   providedIn: 'root',
 })
 export class MealPlanner {
-  private _recipes: Recipe[] = [];
+  recipes$: Observable<Recipe[]>;
 
-  canAddRecipe(recipe: Recipe): boolean {
-    return this._recipes.find((_recipe) => recipe.id === _recipe.id) == null;
-  }
+  private _recipes$ = new BehaviorSubject<Recipe[]>([]);
 
-  getRecipes(): Recipe[] {
-    return this._recipes;
+  constructor() {
+    this.recipes$ = this._recipes$.asObservable();
   }
 
   addRecipe(recipe: Recipe) {
     if (!this.canAddRecipe(recipe)) {
       throw new Error(`Can't add recipe.`);
     }
-    this._recipes = [...this._recipes, recipe];
+    this._recipes$.next([...this._recipes$.value, recipe]);
+  }
+
+  watchCanAddRecipe(recipe: Recipe): Observable<boolean> {
+    return this._recipes$.pipe(
+      map((recipes) => this._canAddRecipes({ recipe, recipes })),
+      distinctUntilChanged()
+    );
+  }
+
+  /**
+   * @deprecated use `watchCanAddRecipe` instead.
+   */
+  canAddRecipe(recipe: Recipe): boolean {
+    return this._canAddRecipes({ recipe, recipes: this.getRecipes() });
+  }
+
+  /**
+   * @deprecated use `recipes$` instead.
+   */
+  getRecipes(): Recipe[] {
+    return this._recipes$.value;
+  }
+
+  private _canAddRecipes({
+    recipes,
+    recipe,
+  }: {
+    recipes: Recipe[];
+    recipe: Recipe;
+  }) {
+    return !recipes.find((_recipe) => _recipe.id === recipe.id);
   }
 }

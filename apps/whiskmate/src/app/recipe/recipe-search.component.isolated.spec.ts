8ffb49306b384d4051f9883ff32ba37717c0e0f5
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-31 16:46:56 +0200

    ✅ add <wm-recipe-search> isolated test

--- apps/whiskmate/src/app/recipe/recipe-search.component.isolated.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.isolated.spec.ts
@@ -1,14 +1,43 @@
+import { of } from 'rxjs';
+import { RecipeRepository } from './recipe-repository.service';
 import { TestBed } from '@angular/core/testing';
 import { RecipeSearchComponent } from './recipe-search.component';
+import { Recipe } from './recipe';
 
 describe(RecipeSearchComponent.name, () => {
-  it.todo('🚧 should search recipes without keyword on load');
+  const papperdelle = { id: 'papperdelle-with-rose-harissa' } as Recipe;
+  const puyLentil = { id: 'puy-lentil-and-aubergine-stew' } as Recipe;
+
+  it('should search recipes without keyword on load', () => {
+    const { component, mockSearch } = createComponent();
+
+    mockSearch.mockReturnValue(of([papperdelle, puyLentil]));
+
+    component.ngOnInit();
+
+    expect(component.recipes).toEqual([papperdelle, puyLentil]);
+
+    expect(mockSearch).toBeCalledTimes(1);
+    expect(mockSearch).toBeCalledWith();
+  });
 
   function createComponent() {
+    const mockSearch = jest.fn() as jest.MockedFunction<
+      typeof RecipeRepository.prototype.search
+    >;
+
     TestBed.configureTestingModule({
-      providers: [RecipeSearchComponent],
+      providers: [
+        RecipeSearchComponent,
+        {
+          provide: RecipeRepository,
+          useValue: {
+            search: mockSearch,
+          },
+        },
+      ],
     });
 
-    return { component: TestBed.inject(RecipeSearchComponent) };
+    return { component: TestBed.inject(RecipeSearchComponent), mockSearch };
   }
 });
--- apps/whiskmate/src/app/recipe/recipe-search.component.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.ts
@@ -1,11 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Recipe } from './recipe';
+import { RecipeRepository } from './recipe-repository.service';
+
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-recipe-search',
   template: `🚧 &lt;wm-recipe-search&gt;`,
 })
-export class RecipeSearchComponent {}
+export class RecipeSearchComponent implements OnDestroy, OnInit {
+  recipes?: Recipe[];
+
+  private _destroyed$ = new ReplaySubject(1);
+
+  constructor(private _recipeRepository: RecipeRepository) {}
+
+  ngOnInit() {
+    this._recipeRepository
+      .search()
+      .pipe(takeUntil(this._destroyed$))
+      .subscribe((recipes) => (this.recipes = recipes));
+  }
+
+  ngOnDestroy() {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
+}
 
 @NgModule({
   declarations: [RecipeSearchComponent],

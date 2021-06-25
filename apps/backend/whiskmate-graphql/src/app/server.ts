87dc2f3615065b57d72ef0b4f54692446589e5bf
Younes Jaaidi <yjaaidi@gmail.com>
2021-06-25 23:38:38 +0200

    🧹 tidy up

---
+++ apps/backend/whiskmate-graphql/src/app/server.ts
@@ -0,0 +1,43 @@
+import {
+  getRecipes,
+  getSimilarRecipes,
+  Recipe,
+} from '@whiskmate/whiskmate-service-core';
+import { ApolloServer, gql } from 'apollo-server';
+import { truncate } from './truncate';
+
+const typeDefs = gql`
+  type Query {
+    creators: [Creator]
+    recipes: [Recipe]
+  }
+
+  type Recipe {
+    creator: Creator
+    name: String
+    ingredients: [Ingredient]
+    similarRecipes: [Recipe]
+  }
+
+  type Ingredient {
+    name: String
+    allergens: [String]
+  }
+
+  type Creator {
+    favoriteIngredients: [Ingredient]
+    recipes: [Recipe]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    recipes: () => getRecipes(),
+  },
+  Recipe: {
+    name: (recipe: Recipe) => truncate(recipe.name, 50),
+    similarRecipes: (recipe: Recipe) => getSimilarRecipes(recipe),
+  },
+};
+
+export const server = new ApolloServer({ typeDefs, resolvers });
--- apps/backend/whiskmate-graphql/src/main.ts
+++ apps/backend/whiskmate-graphql/src/main.ts
@@ -1,52 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-import {
-  getRecipes,
-  getSimilarRecipes,
-  Recipe,
-} from '@whiskmate/whiskmate-service-core';
-import { ApolloServer, gql } from 'apollo-server';
-import { truncate } from './app/truncate';
+import { server } from './app/server';
 
 async function main() {
-  const typeDefs = gql`
-    type Query {
-      creators: [Creator]
-      recipes: [Recipe]
-    }
-
-    type Recipe {
-      creator: Creator
-      name: String
-      ingredients: [Ingredient]
-      similarRecipes: [Recipe]
-    }
-
-    type Ingredient {
-      name: String
-      allergens: [String]
-    }
-
-    type Creator {
-      favoriteIngredients: [Ingredient]
-      recipes: [Recipe]
-    }
-  `;
-
-  const resolvers = {
-    Query: {
-      recipes: () => getRecipes(),
-    },
-    Recipe: {
-      name: (recipe: Recipe) => truncate(recipe.name, 50),
-      similarRecipes: (recipe: Recipe) => getSimilarRecipes(recipe),
-    },
-  };
-
-  const server = new ApolloServer({ typeDefs, resolvers });
-
   const { url } = await server.listen(process.env.port ?? 4000);
   console.log(`🚀  Server ready at ${url}`);
 }

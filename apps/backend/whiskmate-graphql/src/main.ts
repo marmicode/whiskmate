88e62ceb14209a4f28fc0440ac720e3325d5fe36
Younes Jaaidi <yjaaidi@gmail.com>
2021-06-26 02:11:00 +0200

    🛠 change graphql server port to 4001

--- apps/backend/whiskmate-graphql/src/main.ts
+++ apps/backend/whiskmate-graphql/src/main.ts
@@ -1,7 +1,7 @@
 import { server } from './app/server';
 
 async function main() {
-  const { url } = await server.listen(process.env.port ?? 4000);
+  const { url } = await server.listen(process.env.port ?? 4001);
   console.log(`🚀  Server ready at ${url}`);
 }
 

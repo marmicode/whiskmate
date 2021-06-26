f0dce268ff55530273717a1fe78e8e7ce2826349
Younes Jaaidi <yjaaidi@gmail.com>
2021-06-26 16:29:25 +0200

    ✅ add request validation using openapi

--- apps/backend/whiskmate-restful/src/main.ts
+++ apps/backend/whiskmate-restful/src/main.ts
@@ -1,12 +1,31 @@
+import { join } from 'path';
 import { json } from 'body-parser';
 import * as express from 'express';
 import { recipesRouter } from './app/recipes.router';
+import { middleware as openapiValidator } from 'express-openapi-validator';
 
 const app = express();
 
 app.use(json({ type: 'application/json' }));
+app.use(
+  openapiValidator({
+    apiSpec: join(__dirname, 'assets/whiskmate.yaml'),
+    validateRequests: true,
+  })
+);
 
 app.use('/recipes', recipesRouter);
+
+app.use((err, req, res, next) => {
+  const errorType = `https://errors.whiskmate.io/${
+    err.errors?.[0]?.errorCode ?? 'unknown-error'
+  }`;
+  res.status(err.status ?? 500).json({
+    type: errorType,
+    title: err.message,
+    errors: err.errors,
+  });
+});
 
 const port = process.env.port ?? 4000;
 const server = app.listen(port, () => {

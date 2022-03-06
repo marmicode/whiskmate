fe6fba2b2f088e1880a9c429932347a159d3799f
Younes Jaaidi <yjaaidi@gmail.com>
2022-03-06 17:34:58 +0100

    feat: ✅ reject invalid requests

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,35 @@ function main() {
 
   app.use(json());
 
+  app.use(
+    openapiMiddleware({
+      apiSpec: join(__dirname, 'openapi/whiskmate.yaml'),
+      validateRequests: true,
+    })
+  );
+
   app.use(recipesRouter);
 
   app.use((err: unknown, req: unknown, res: Response, next: NextFunction) => {
     console.error(err);
+
+    if (err instanceof error.BadRequest) {
+      res.status(err.status).send({
+        type: 'https://whiskmate.io/problems/bad-request',
+        detail: err.message,
+        errors: err.errors,
+      });
+      return;
+    }
+
+    if (err instanceof error.NotFound) {
+      res.status(err.status).send({
+        type: 'https://whiskmate.io/problems/not-found',
+        detail: err.message,
+      });
+      return;
+    }
+
     res.status(500).send({
       type: 'https://whiskmate.io/problems/unknown-error',
       title: 'Unknown Error',

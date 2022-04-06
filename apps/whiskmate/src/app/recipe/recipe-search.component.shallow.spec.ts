0a7e5dd18e526ff7de0d567f59891b212fb8c032
Younes Jaaidi <yjaaidi@gmail.com>
2022-04-06 22:55:55 +0200

    refactor: 🧹 remove useless import

--- apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
@@ -1,8 +1,7 @@
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { create } from 'domain';
 import { of } from 'rxjs';
 import { MealPlanner } from './../meal-planner/meal-planner.service';
 import { Recipe } from './recipe';

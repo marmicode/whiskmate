37b8c9087c6be82c8b1c56e674edeb57b81b8ee1
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-12 17:33:34 +0100

    refactor: 🛠 switch to standalone components

--- apps/whiskmate/src/app/app.component.ts
+++ apps/whiskmate/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
   selector: 'wm-app',
   template: `<h1>Welcome to Whismate</h1>`,
 })

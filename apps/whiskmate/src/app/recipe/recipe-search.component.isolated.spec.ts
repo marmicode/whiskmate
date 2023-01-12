33ec0fae148a1aa3bc43b147dfd538cc168852a1
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-12 17:58:56 +0100

    test: ✅ simplify test boilerplate

--- apps/whiskmate/src/app/recipe/recipe-search.component.isolated.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.isolated.spec.ts
@@ -2,19 +2,15 @@ import { TestBed } from '@angular/core/testing';
 import { RecipeSearchComponent } from './recipe-search.component';
 
 describe(RecipeSearchComponent.name, () => {
-  it.todo('🚧 should search recipes without keyword on load');
+  it.todo('🚧 should search recipes without filtering');
 
   function createComponent() {
     TestBed.configureTestingModule({
       providers: [RecipeSearchComponent],
     });
 
-    return {
-      render() {
-        const component = TestBed.inject(RecipeSearchComponent);
-        component.ngOnInit();
-        return { component };
-      },
-    };
+    const component = TestBed.inject(RecipeSearchComponent);
+    component.ngOnInit();
+    return { component };
   }
 });

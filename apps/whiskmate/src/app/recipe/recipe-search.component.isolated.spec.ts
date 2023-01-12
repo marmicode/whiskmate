edcc179792a0939343834826c629db977b163b33
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-12 18:01:32 +0100

    Revert "test: ✅ simplify test boilerplate"
    This reverts commit 33ec0fae148a1aa3bc43b147dfd538cc168852a1.

--- apps/whiskmate/src/app/recipe/recipe-search.component.isolated.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.isolated.spec.ts
@@ -2,15 +2,19 @@ import { TestBed } from '@angular/core/testing';
 import { RecipeSearchComponent } from './recipe-search.component';
 
 describe(RecipeSearchComponent.name, () => {
-  it.todo('🚧 should search recipes without filtering');
+  it.todo('🚧 should search recipes without keyword on load');
 
   function createComponent() {
     TestBed.configureTestingModule({
       providers: [RecipeSearchComponent],
     });
 
-    const component = TestBed.inject(RecipeSearchComponent);
-    component.ngOnInit();
-    return { component };
+    return {
+      render() {
+        const component = TestBed.inject(RecipeSearchComponent);
+        component.ngOnInit();
+        return { component };
+      },
+    };
   }
 });

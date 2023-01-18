45a38cf40992a720c23bfe62dfd81c35c4ee5110
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-18 23:11:02 +0100

    test: ✅ add RecipePreviewComponent test boilerplate

---
+++ apps/whiskmate/src/app/recipe/recipe-preview.component.cy.ts
@@ -0,0 +1,10 @@
+import { RecipePreviewComponent } from './recipe-preview.component';
+import { recipeMother } from '../testing/recipe.mother';
+
+describe(RecipePreviewComponent.name, () => {
+  xit('🚧 should show recipe name', () => {
+    cy.mount(RecipePreviewComponent);
+
+    throw new Error('🚧 work in progress!');
+  });
+});

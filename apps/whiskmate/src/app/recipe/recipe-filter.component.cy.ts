ca5523a14bd4f7dfcc755a8a73f437a6096ddacb
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-19 00:44:49 +0100

    test: ✅ add test for RecipeFilterComponent

--- apps/whiskmate/src/app/recipe/recipe-filter.component.cy.ts
+++ apps/whiskmate/src/app/recipe/recipe-filter.component.cy.ts
@@ -1,8 +1,36 @@
 import { RecipeFilterComponent } from './recipe-filter.component';
+import { RecipeFilter } from './recipe-filter';
 import { spyOutput } from '../../testing/cypress-observer';
+import { SinonStub } from 'cypress/types/sinon';
 
 describe(RecipeFilterComponent.name, () => {
-  xit('🚧 should trigger filterChange output', () => {
-    throw new Error('🚧 work in progress!');
+  it('should trigger filterChange output', () => {
+    const { getFilterChangeSpy, typeInput } = renderComponent();
+
+    typeInput('Keywords', 'Burger');
+    typeInput('Max Ingredients', '3');
+    typeInput('Max Steps', '5');
+
+    getFilterChangeSpy().should('have.been.calledWith', {
+      keywords: 'Burger',
+      maxIngredientCount: 3,
+      maxStepCount: 5,
+    });
   });
+
+  function renderComponent() {
+    cy.mount(RecipeFilterComponent).then(spyOutput('filterChange'));
+
+    return {
+      typeInput(
+        inputLabel: 'Keywords' | 'Max Ingredients' | 'Max Steps',
+        keywords: string
+      ) {
+        cy.findByLabelText(inputLabel).type(keywords, { force: true });
+      },
+      getFilterChangeSpy() {
+        return cy.get<SinonStub<[RecipeFilter]>>('@filterChange');
+      },
+    };
+  }
 });

4bd799736e226d3a1ca5214279be31c56ea0743c
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-19 00:42:01 +0100

    test: ✅ add empty test for RecipeFilterComponent

---
+++ apps/whiskmate/src/app/recipe/recipe-filter.component.cy.ts
@@ -0,0 +1,8 @@
+import { RecipeFilterComponent } from './recipe-filter.component';
+import { spyOutput } from '../../testing/cypress-observer';
+
+describe(RecipeFilterComponent.name, () => {
+  xit('🚧 should trigger filterChange output', () => {
+    throw new Error('🚧 work in progress!');
+  });
+});

c90e2e1775b95f0e786f33de33ba0f8f2308ce54
Younes Jaaidi <yjaaidi@gmail.com>
2023-01-19 12:43:50 +0100

    test: ✅ set up RecipePreviewComponent CT harness

--- apps/whiskmate/src/app/recipe/recipe-preview.component.cy.ts
+++ apps/whiskmate/src/app/recipe/recipe-preview.component.cy.ts
@@ -1,14 +1,25 @@
 import { RecipePreviewComponent } from './recipe-preview.component';
 import { recipeMother } from '../testing/recipe.mother';
+import { getHarness } from '@jscutlery/cypress-harness';
+import { RecipePreviewHarness } from './recipe-preview.harness';
 
 describe(RecipePreviewComponent.name, () => {
-  it('should show recipe name', () => {
-    cy.mount(RecipePreviewComponent, {
+  xit('should show recipe name', () => {
+    const { harness } = renderComponent();
+
+    throw new Error('🚧 work in progress!');
+  });
+
+  function renderComponent() {
+    cy.mount('<wm-recipe-preview [recipe]="recipe"></wm-recipe-preview>', {
+      imports: [RecipePreviewComponent],
       componentProperties: {
         recipe: recipeMother.withBasicInfo('Burger').build(),
       },
     });
 
-    cy.findByRole('heading').should('have.text', 'Burger');
-  });
+    return {
+      harness: getHarness(RecipePreviewHarness),
+    };
+  }
 });

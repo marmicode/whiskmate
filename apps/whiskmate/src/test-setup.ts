e89998af62e1697ab10519f7c581a503dbc3ef5f
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-03 00:14:11 +0100

    test: ✅ speed up tests with swc

--- apps/whiskmate/src/test-setup.ts
+++ apps/whiskmate/src/test-setup.ts
@@ -1,4 +1,12 @@
-import 'jest-preset-angular/setup-jest';
+import 'zone.js';
+import 'zone.js/testing';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => {};
+const Zone = (globalThis as any)['Zone'] as any;
+Zone['ProxyZoneSpec'] = {
+  assertPresent: () => ({ onHasTask: noop, resetDelegate: noop }),
+};
 
 import { getTestBed } from '@angular/core/testing';
 import {

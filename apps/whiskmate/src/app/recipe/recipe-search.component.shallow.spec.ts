c6bc25fafeec92e8e0a2efe4bf913b17761af31e
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-03 14:59:40 +0100

    test: ✅ use provideRecipeRepositoryFake

--- apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
@@ -1,30 +1,23 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { By } from '@angular/platform-browser';
 import { TestBed } from '@angular/core/testing';
-import { RecipeRepository } from './recipe-repository.service';
 import { RecipeSearchComponent } from './recipe-search.component';
-import { RecipeRepositoryFake } from './recipe-repository.fake';
+import {
+  provideRecipeRepositoryFake,
+  RecipeRepositoryFake,
+} from './recipe-repository.fake';
 import { recipeMother } from '../testing/recipe.mother';
 
 describe(RecipeSearchComponent.name, () => {
   it.todo('🚧 should search recipes without filtering');
 
   function renderComponent() {
-    const fakeRepo = new RecipeRepositoryFake();
+    TestBed.configureTestingModule({
+      providers: [provideRecipeRepositoryFake()],
+    });
 
-    fakeRepo.setRecipes([
+    TestBed.inject(RecipeRepositoryFake).setRecipes([
       recipeMother.withBasicInfo('Burger').build(),
       recipeMother.withBasicInfo('Salad').build(),
     ]);
-
-    TestBed.configureTestingModule({
-      providers: [
-        {
-          provide: RecipeRepository,
-          useValue: fakeRepo,
-        },
-      ],
-    });
 
     const fixture = TestBed.createComponent(RecipeSearchComponent);
     fixture.detectChanges();

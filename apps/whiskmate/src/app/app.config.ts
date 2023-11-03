40fca4757ef36affe8f59704b8a4de313ff936ca
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-03 11:43:53 +0100

    refactor: 🛠 fix lint warnings

--- apps/whiskmate/src/app/app.config.ts
+++ apps/whiskmate/src/app/app.config.ts
@@ -1,7 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),

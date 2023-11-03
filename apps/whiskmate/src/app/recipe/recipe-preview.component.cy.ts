95b19113be3a611cbaaa560d93576fe90f2ec0e6
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-03 17:25:11 +0100

    test: ✅ add recipe preview component test starter

---
+++ apps/whiskmate/src/app/recipe/recipe-preview.component.cy.ts
@@ -0,0 +1,7 @@
+import { RecipePreviewComponent } from './recipe-preview.component';
+
+describe(RecipePreviewComponent.name, () => {
+  xit('🚧 should show recipe name', () => {
+    cy.mount(RecipePreviewComponent);
+  });
+});

515007730ea618703539184623c924a4dad547f2
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-03 17:06:12 +0100

    refactor: 🛠 add safeSignal

--- apps/whiskmate/src/app/meal-planner/recipe-add-button.component.ts
+++ apps/whiskmate/src/app/meal-planner/recipe-add-button.component.ts
@@ -1,15 +1,14 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  effect,
   inject,
   Input,
-  signal,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Recipe } from '../recipe/recipe';
 import { rxComputed } from '@jscutlery/rx-computed';
 import { MealPlanner } from './meal-planner.service';
+import { safeSignal } from '../shared/safe-signal';
 
 @Component({
   standalone: true,
@@ -37,25 +36,15 @@ import { MealPlanner } from './meal-planner.service';
   ],
 })
 export class RecipeAddButtonComponent {
-  @Input({ required: true }) set recipe(recipe: Recipe) {
-    this._recipe.set(recipe);
+  @Input({ alias: 'recipe', required: true }) set _recipe(recipe: Recipe) {
+    this.recipe.set(recipe);
   }
-
-  canAdd = rxComputed(() =>
-    this._mealPlanner.watchCanAddRecipe(this.recipeSig())
-  );
-  recipeSig = () => {
-    const recipe = this._recipe();
-    if (recipe === undefined) {
-      throw new Error('signal read before initialization');
-    }
-    return recipe;
-  };
+  canAdd = rxComputed(() => this._mealPlanner.watchCanAddRecipe(this.recipe()));
+  recipe = safeSignal<Recipe>();
 
   private _mealPlanner = inject(MealPlanner);
-  private _recipe = signal<Recipe | undefined>(undefined);
 
   addRecipe() {
-    this._mealPlanner.addRecipe(this.recipeSig());
+    this._mealPlanner.addRecipe(this.recipe());
   }
 }

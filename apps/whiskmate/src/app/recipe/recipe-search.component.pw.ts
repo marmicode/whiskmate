6065654d2e9deb740dbc48656092e6faf4b58a1f
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-04 16:30:35 +0100

    test: ✅ add recipe search playwright test

--- apps/whiskmate/src/app/recipe/recipe-search.component.pw.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.pw.ts
@@ -1,21 +1,51 @@
 import { expect, test } from '@jscutlery/playwright-ct-angular';
+import { RecipeSearchTestContainerComponent } from './recipe-search.test-container';
+import { recipeMother } from '../testing/recipe.mother';
 
 test.describe('<wm-recipe-search>', () => {
   test('should show recipes', async ({ mount }) => {
-    test.skip(true, '🚧 work in progress!');
+    const component = await mount(RecipeSearchTestContainerComponent);
+
+    await expect(component.getByRole('heading', { level: 2 })).toHaveText([
+      'Burger',
+      'Salad',
+    ]);
   });
 
   test('should filter recipes', async ({ mount }) => {
-    test.skip(true, '🚧 work in progress!');
+    const component = await mount(RecipeSearchTestContainerComponent);
+
+    await component.getByLabel('Keywords').fill('Bur');
+
+    await expect(component.getByRole('heading', { level: 2 })).toHaveText([
+      'Burger',
+    ]);
   });
 
   test('should add recipe to meal plan', async ({ mount }) => {
-    test.skip(true, '🚧 work in progress!');
+    const component = await mount(RecipeSearchTestContainerComponent, {
+      spyOutputs: ['mealPlannerRecipesChange'],
+    });
+
+    await component.getByRole('button', { name: 'ADD' }).first().click();
+
+    /* There is only a burger in the meal planner. */
+    expect(component.spies.mealPlannerRecipesChange).toHaveBeenLastCalledWith([
+      expect.objectContaining({ name: 'Burger' }),
+    ]);
   });
 
   test('should disable add button if recipe is already in meal plan', async ({
     mount,
   }) => {
-    test.skip(true, '🚧 work in progress!');
+    const component = await mount(RecipeSearchTestContainerComponent, {
+      inputs: {
+        mealPlannerRecipes: [recipeMother.withBasicInfo('Burger').build()],
+      },
+    });
+
+    await expect(
+      component.getByRole('button', { name: 'ADD' }).first()
+    ).toBeDisabled();
   });
 });
--- apps/whiskmate/src/app/recipe/recipe-search.test-container.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.test-container.ts
@@ -1,24 +1,44 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Input,
+  Output,
+} from '@angular/core';
 import { RecipeSearchComponent } from './recipe-search.component';
 import {
   provideRecipeRepositoryFake,
   RecipeRepositoryFake,
 } from './recipe-repository.fake';
 import { recipeMother } from '../testing/recipe.mother';
+import { MealPlanner } from '../meal-planner/meal-planner.service';
+import { Recipe } from './recipe';
+import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-recipe-search-test-container',
+
   providers: [provideRecipeRepositoryFake()],
   imports: [RecipeSearchComponent],
   template: `
       <wm-recipe-search/>`,
 })
 export class RecipeSearchTestContainerComponent {
+  @Input() set mealPlannerRecipes(meals: Recipe[]) {
+    for (const meal of meals) {
+      this._mealPlanner.addRecipe(meal);
+    }
+  }
+
+  @Output() mealPlannerRecipesChange: Observable<Recipe[]>;
+
+  private _mealPlanner = inject(MealPlanner);
   private _recipeRepositoryFake = inject(RecipeRepositoryFake);
 
   constructor() {
+    this.mealPlannerRecipesChange = this._mealPlanner.recipes$;
     this._recipeRepositoryFake.setRecipes([
       recipeMother.withBasicInfo('Burger').build(),
       recipeMother.withBasicInfo('Salad').build(),

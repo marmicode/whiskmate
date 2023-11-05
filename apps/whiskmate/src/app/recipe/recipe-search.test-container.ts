000d9156dfb9bd3981d754c706d448ccf93727c9
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-05 20:08:39 +0100

    test: ✅ remove fake setup and let it be part of the exercise

--- apps/whiskmate/src/app/recipe/recipe-search.test-container.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.test-container.ts
@@ -1,27 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RecipeSearchComponent } from './recipe-search.component';
-import {
-  provideRecipeRepositoryFake,
-  RecipeRepositoryFake,
-} from './recipe-repository.fake';
-import { recipeMother } from '../testing/recipe.mother';
 
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-recipe-search-test-container',
-  providers: [provideRecipeRepositoryFake()],
   imports: [RecipeSearchComponent],
   template: `
       <wm-recipe-search/>`,
 })
-export class RecipeSearchTestContainerComponent {
-  private _recipeRepositoryFake = inject(RecipeRepositoryFake);
-
-  constructor() {
-    this._recipeRepositoryFake.setRecipes([
-      recipeMother.withBasicInfo('Burger').build(),
-      recipeMother.withBasicInfo('Salad').build(),
-    ]);
-  }
-}
+export class RecipeSearchTestContainerComponent {}

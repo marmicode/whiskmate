4c2e396b805eb6dfa1803c584f260c0ac89ea972
Younes Jaaidi <younes@marmicode.io>
2024-03-15 12:38:00 +0100

    docs: 📝 rename branch testing-501-recipe-preview-ct to testing-501-recipe-preview-ct-solution

--- tools/sync-testing.sh
+++ tools/sync-testing.sh
@@ -42,7 +42,7 @@ ${TOOLS_PATH}/cascade-changes.sh \
   testing-404-recipe-filter-material-starter \
   testing-404-recipe-filter-material-solution \
   testing-500-recipe-preview-ct-starter \
-  testing-501-recipe-preview-ct \
+  testing-501-recipe-preview-ct-solution \
   testing-502-recipe-filter-ct-starter \
   testing-503-recipe-filter-ct \
   testing-504-recipe-search-ct-starter \

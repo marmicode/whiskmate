a73d0ffa947d55fbdd398a29961581f1b29c713b
Younes Jaaidi <younes@marmicode.io>
2024-05-10 15:25:07 +0200

    refactor: 🛠️ remove useless import

--- apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -26,7 +25,7 @@ describe(RecipeSearchComponent.name, () => {
 
     TestBed.overrideComponent(RecipeSearchComponent, {
       set: {
-        imports: [CommonModule],
+        imports: [],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
       },
     });

7eb3b76236c320b8493561c049af626f302e292c
Younes Jaaidi <younes@marmicode.io>
2024-05-10 15:29:24 +0200

    test: ✅ fix missing import

--- apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.shallow.spec.ts
@@ -8,6 +8,7 @@ import {
   RecipeRepositoryFake,
 } from './recipe-repository.fake';
 import { render } from '@testing-library/angular';
+import { AsyncPipe } from '@angular/common';
 
 describe(RecipeSearchComponent.name, () => {
   it('should search recipes without filtering', async () => {
@@ -25,7 +26,7 @@ describe(RecipeSearchComponent.name, () => {
       configureTestBed(testBed) {
         testBed.overrideComponent(RecipeSearchComponent, {
           set: {
-            imports: [],
+            imports: [AsyncPipe],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
           },
         });

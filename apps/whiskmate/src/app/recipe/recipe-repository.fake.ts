f500f626c0d4e1cd4cebebca0a7239eb525f8d7a
Younes Jaaidi <younes@marmicode.io>
2024-09-21 17:58:53 +0200

    test: ✅ filter recipes on observable subscription

--- apps/whiskmate/src/app/recipe/recipe-repository.fake.ts
+++ apps/whiskmate/src/app/recipe/recipe-repository.fake.ts
@@ -1,6 +1,6 @@
 import { Injectable, Provider } from '@angular/core';
 import { RecipeFilter } from './recipe-filter';
-import { Observable, of } from 'rxjs';
+import { defer, Observable, of } from 'rxjs';
 import { Recipe } from './recipe';
 import {
   RecipeRepository,
@@ -18,25 +18,26 @@ export class RecipeRepositoryFake implements RecipeRepositoryDef {
     maxIngredientCount,
     maxStepCount,
   }: RecipeFilter = {}): Observable<Recipe[]> {
-    const recipes = this._recipes.filter((recipe) => {
-      const conditions = [
-        /* Filter by keywords. */
-        () => (keywords ? recipe.name.includes(keywords) : true),
-        /* Filter by max ingredients. */
-        () =>
-          maxIngredientCount != null
-            ? recipe.ingredients.length <= maxIngredientCount
-            : true,
-        /* Filter by max steps. */
-        () =>
-          maxStepCount != null ? recipe.steps.length <= maxStepCount : true,
-      ];
+    return defer(() => {
+      const recipes = this._recipes.filter((recipe) => {
+        const conditions = [
+          /* Filter by keywords. */
+          () => (keywords ? recipe.name.includes(keywords) : true),
+          /* Filter by max ingredients. */
+          () =>
+            maxIngredientCount != null
+              ? recipe.ingredients.length <= maxIngredientCount
+              : true,
+          /* Filter by max steps. */
+          () =>
+            maxStepCount != null ? recipe.steps.length <= maxStepCount : true,
+        ];
 
-      /* Return true if all conditions are true. */
-      return conditions.every((condition) => condition());
+        /* Return true if all conditions are true. */
+        return conditions.every((condition) => condition());
+      });
+      return of(recipes);
     });
-
-    return of(recipes);
   }
 
   setRecipes(recipes: Recipe[]) {

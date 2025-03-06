3f0fc24adeef389662f7178aa518d7e9877006d7
Younes Jaaidi <younes@marmicode.io>
2025-03-06 17:53:34 +0100

    refactor: 🛠️ remove standalone flag

--- apps/whiskmate/src/app/app.component.ts
+++ apps/whiskmate/src/app/app.component.ts
@@ -2,10 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TitleComponent } from './shared/title.component';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: true,
-  selector: 'wm-app',
-  imports: [TitleComponent],
-  template: ` <wm-title>👨🏻‍🍳 Welcome to Whiskmate 🥘</wm-title> `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'wm-app',
+    imports: [TitleComponent],
+    template: ` <wm-title>👨🏻‍🍳 Welcome to Whiskmate 🥘</wm-title> `
 })
 export class AppComponent {}

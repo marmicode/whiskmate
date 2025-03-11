b6a9477c2a61c687b57c9dc8f92a88a90521d032
Younes Jaaidi <younes@marmicode.io>
2025-03-11 17:59:45 +0100

    refactor: 🛠️ remove standalone flag

--- apps/whiskmate/src/app/recipe/recipe-search.test-container.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.test-container.ts
@@ -2,11 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RecipeSearchComponent } from './recipe-search.component';
 
 @Component({
-  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-recipe-search-test-container',
   imports: [RecipeSearchComponent],
-  template: `
-      <wm-recipe-search/>`,
+  template: `<wm-recipe-search/>`,
 })
 export class RecipeSearchTestContainerComponent {}

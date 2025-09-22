495137dc2c2462fdb531d26dcb63d17d8343b79d
Younes Jaaidi <younes@marmicode.io>
2025-09-22 21:07:25 +0200

    test: ✅ remove cypress starter and rename playwright one

--- apps/whiskmate/src/app/recipe/recipe-filter.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { RecipeFilterComponent } from './recipe-filter.component';
-
-describe(RecipeFilterComponent.name, () => {
-  xit('🚧 should trigger filterChange output', () => {
-    throw new Error('😱 Not implemented yet!');
-  });
-});

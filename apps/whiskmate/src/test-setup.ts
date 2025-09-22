699a7d5fb70594312c415f1ebf716f04a1f35e4f
Younes Jaaidi <younes@marmicode.io>
2025-09-22 15:09:36 +0200

    test: ✅ factorize test setup between jest & vitest

--- apps/whiskmate/src/test-setup.ts
+++ apps/whiskmate/src/test-setup.ts
@@ -1,22 +1,2 @@
 import '@testing-library/jest-dom/vitest';
-
-import { getTestBed, TestBed } from '@angular/core/testing';
-
-import './styles.css';
-
-import {
-  BrowserTestingModule,
-  platformBrowserTesting,
-} from '@angular/platform-browser/testing';
-import { provideZonelessChangeDetection } from '@angular/core';
-
-getTestBed().initTestEnvironment(
-  BrowserTestingModule,
-  platformBrowserTesting(),
-);
-
-beforeEach(() => {
-  TestBed.configureTestingModule({
-    providers: [provideZonelessChangeDetection()],
-  });
-});
+import './test-setup-common';

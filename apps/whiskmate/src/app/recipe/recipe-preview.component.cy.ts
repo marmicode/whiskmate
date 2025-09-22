77efb1e12562180be287d7fea4c8b0077e0134cc
Younes Jaaidi <younes@marmicode.io>
2025-09-22 21:05:49 +0200

    test: ✅ remove cypress starter and rename playwright one

--- apps/whiskmate/src/app/recipe/recipe-preview.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { RecipePreviewComponent } from './recipe-preview.component';
-
-describe(RecipePreviewComponent.name, () => {
-  xit('🚧 should show recipe name', () => {
-    cy.mount(RecipePreviewComponent);
-  });
-});

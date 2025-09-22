2f6a02c2bbdbcbe7c63b276cab16da7bbda9da32
Younes Jaaidi <younes@marmicode.io>
2025-09-22 15:09:36 +0200

    test: ✅ factorize test setup between jest & vitest

---
+++ apps/whiskmate/src/test-setup-common.ts
@@ -0,0 +1,19 @@
+import './styles.css';
+
+import { provideZonelessChangeDetection } from '@angular/core';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting(),
+);
+
+beforeEach(() => {
+  getTestBed().configureTestingModule({
+    providers: [provideZonelessChangeDetection()],
+  });
+});
--- apps/whiskmate/src/test-setup-jest.ts
+++ apps/whiskmate/src/test-setup-jest.ts
@@ -1,25 +1,7 @@
 import '@testing-library/jest-dom';
-
-import { provideZonelessChangeDetection } from '@angular/core';
-import { getTestBed, TestBed } from '@angular/core/testing';
-import {
-  BrowserTestingModule,
-  platformBrowserTesting,
-} from '@angular/platform-browser/testing';
-
-getTestBed().initTestEnvironment(
-  BrowserTestingModule,
-  platformBrowserTesting(),
-);
-
-beforeEach(() => {
-  TestBed.configureTestingModule({
-    providers: [provideZonelessChangeDetection()],
-  });
-});
+import './test-setup-common';
 
 const originalItTodo = it.todo.bind(it);
 /* Strip extra arguments to align with vitest and avoid the following error:
  * "todo must be called with only a description." */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-it.todo = ((name: string) => originalItTodo(name)) as any;
+it.todo = (name: string) => originalItTodo(name);
